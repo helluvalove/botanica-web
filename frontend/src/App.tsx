b625459578c1969bb5760d592bc2bf3a82c979ca
helluvalove <kisequ@vk.com>
2025-10-29 13:01:43 +0300

    initial frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Botanica - Кафе-кальянная</h1>
+      <p>Сайт в разработке</p>
+    </div>
+  );
+}
+
+export default App;
